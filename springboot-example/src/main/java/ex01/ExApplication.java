2b0597cc68c76df163677d29087c61fb403c35d7
wizdom972 <jihae0584@gmail.com>
2025-01-20 17:01:01 +0900

    📝 2025-01-20 수업 내용
    - 스프링부트 시작

---
+++ springboot-example/src/main/java/ex01/ExApplication.java
@@ -0,0 +1,38 @@
+package ex01;
+
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExApplication {
+
+	@Bean
+	public Map<?, ?> myMap() {
+		return Map.of("greetings", "Hello World!");
+	}
+
+	public static void main(String[] args) {
+
+//		ConfigurableApplicationContext ac = null;
+//		
+//		try {
+//			ac = SpringApplication.run(ExApplication.class, args);
+//			System.out.println("ExApplication runs...." + ac.getBean("myMap", Map.class).get("greetings"));
+//		
+//		} catch (Exception ignore) {
+//		
+//		} finally {
+//			ac.close();
+//		}
+
+		try (ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args)) {
+			System.out.println("ExApplication runs... " + ac.getBean("myMap", Map.class).get("greetings"));
+		}
+
+	}
+
+}
